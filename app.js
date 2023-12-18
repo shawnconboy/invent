197c8d4f220d2f37cfa8a1cb6af0a09ea8552ab8
Shawn Conboy <conboysbusiness@gmail.com>
2023-12-17 21:42:32 -0500

    Allows user to add task to list of tasks

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 let addTaskBtn = document.getElementById('addTaskBtn');
+let taskList = document.getElementById('taskListItems');
 
-document.addEventListener('click', function () {
+function capitalizeWords(str) {
+    return str.replace(/\b\w/g, function (char) {
+        return char.toUpperCase();
+    });
+}
 
-})
+addTaskBtn.addEventListener('click', function () {
+    let task = prompt("Add a task.");
+
+    if (task !== null && task.trim() !== '') {
+        let capitalizedTask = capitalizeWords(task);
+        let newTask = document.createElement('li');
+        newTask.textContent = capitalizedTask;
+        taskList.appendChild(newTask);
+    }
+});
