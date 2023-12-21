1ab375cd43d31ddb658fe64610679852ccb09fc4
Shawn Conboy <conboysbusiness@gmail.com>
2023-12-21 03:24:37 -0500

    Fix styling of 'add task' items so they're all the same

--- app.js
+++ app.js
@@ -12,15 +12,33 @@ addTaskBtn.addEventListener('click', function () {
 
     if (task !== null && task.trim() !== '') {
         let capitalizedTask = capitalizeWords(task);
+
+        let newTaskDiv = document.createElement('div');
+        newTaskDiv.classList.add('taskListItems');
+
         let newTask = document.createElement('li');
         newTask.textContent = capitalizedTask;
-        taskList.appendChild(newTask);
+
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('deleteButton');
+        deleteButton.textContent = 'x';
+
+        deleteButton.addEventListener('click', function () {
+            newTaskDiv.remove();
+        });
+
+        // Add the newTask and deleteButton to newTaskDiv
+        newTaskDiv.appendChild(newTask);
+        newTaskDiv.appendChild(deleteButton);
+
+        taskList.appendChild(newTaskDiv);
     }
 });
 
-let deleteButton = document.querySelectorAll('.deleteButton');
+// Existing code for deleting tasks
+let deleteButtons = document.querySelectorAll('.deleteButton');
 
-deleteButton.forEach(button => {
+deleteButtons.forEach(button => {
     button.addEventListener('click', function () {
         let parentDiv = button.parentNode;
         parentDiv.remove();
